5812c0a4b3de48dffdf39cdb02e945b81ce743a2
Sergey Ukhanov <ukhanov.sergey@gmail.com>
2020-09-20 22:02:06 +0600

    Local storage for state storing

--- src/store/store.js
+++ src/store/store.js
@@ -5,20 +5,28 @@ import {createStore, applyMiddleware} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import {ActionCreator} from '../reducers/service/service';
+import {loadState, saveState} from '../utils/utils';
 
 const onError = (error) => {
   store.dispatch(ActionCreator.setError(error));
 };
 
 
+const presistedState = loadState();
+
 const api = createAPI(onError);
 
 const store = createStore(
     reducer,
+    presistedState,
     composeWithDevTools(
         applyMiddleware(thunk.withExtraArgument(api))
     )
 );
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
 
 export default store;
 
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -47,3 +47,29 @@ export const prepareOrderData = (state, cart, currency, deliveryFee) => {
   return Object.assign({}, state, {cart});
 };
 
+
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem(`savedState`);
+    if (serializedState === null) {
+      return undefined;
+    }
+
+    const stateToReturn = JSON.parse(serializedState);
+    return stateToReturn;
+  } catch (err) {
+    return undefined;
+  }
+};
+
+export const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem(`savedState`, serializedState);
+    return true;
+  } catch (err) {
+    return undefined;
+  }
+};
+
+
