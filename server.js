148b88c163a1a5ae35e48702e88cff194c5a028e
Sergey Ukhanov <ukhanov.sergey@gmail.com>
2020-09-21 23:59:37 +0600

    Small server script: changed uri pattern

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
-const express = require('express');
-const path = require('path');
+const express = require(`express`);
+const path = require(`path`);
 const app = express();
-app.use(express.static(__dirname + '/public'));
+
+app.use(express.static(path.join(__dirname, `public`)));
+
+
+app.get(`/*`, function (req, res) {
+  res.sendFile(path.join(__dirname, `public`, `index.html`), function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
 app.listen(80);
-console.log('Pizza server started');
+
+console.log(`Pizza server started`);
